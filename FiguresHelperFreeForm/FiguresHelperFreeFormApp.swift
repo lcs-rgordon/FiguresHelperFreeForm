38beb88c9690c8659b029400d418ed3d89630127
Russell Gordon <rgordon@lcs.on.ca>
2021-12-04 06:30:06 -0500

    Initial Commit

---
+++ FiguresHelperFreeForm/FiguresHelperFreeFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiguresHelperFreeFormApp.swift
+//  FiguresHelperFreeForm
+//
+//  Created by Russell Gordon on 2021-12-04.
+//
+
+import SwiftUI
+
+@main
+struct FiguresHelperFreeFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
